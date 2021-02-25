5b1ca262415227a992e080739da050055902284b
SoyTiyi <santiago.marmartinez@gmail.com>
2021-02-25 07:30:36 -0500

    Add ThreadCovidAnalyzer class

---
+++ covidAnalyzerTool/src/main/java/eci/arsw/covidanalyzer/ThreadCovidAnalyzer.java
@@ -0,0 +1,49 @@
+package eci.arsw.covidanalyzer;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadCovidAnalyzer extends Thread {
+
+    private boolean run;
+    private AtomicInteger amountOfFilesProcessed;
+    private List<File> resultFiles;
+    private TestReader testReader;
+    private ResultAnalyzer resultAnalyzer;
+    private int first;
+    private int last;
+
+    public ThreadCovidAnalyzer(AtomicInteger amountOfFilesProcessed, List<File> resultFiles, TestReader testReader,
+            ResultAnalyzer resultAnalyzer, int first, int last) {
+        this.amountOfFilesProcessed = amountOfFilesProcessed;
+        this.resultFiles = resultFiles;
+        this.testReader = testReader;
+        this.resultAnalyzer = resultAnalyzer;
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public void run() {
+        for (int i = first; i < last; i++) {
+            List<Result> results = testReader.readResultsFromFile(resultFiles.get(i));
+            for (Result result : results) {
+                resultAnalyzer.addResult(result);
+            }
+            amountOfFilesProcessed.incrementAndGet();
+        }
+    }
+
+    public synchronized void pauseState(boolean run){
+        this.run = run;
+        if(run){
+            this.notifyAll();
+        }
+    }
+
+    public boolean getRun(){
+        return this.run;
+    }
+
+}
